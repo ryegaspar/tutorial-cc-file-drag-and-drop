9c9a8ef22612ed5f5cefe7774c0b1a334cafe044
rye gaspar <gaspar.ryan@outlook.com>
2020-07-05 20:20:53 -0500

    08 - uploads status bar styling

--- client/src/components/Uploads.vue
+++ client/src/components/Uploads.vue
@@ -1,5 +1,12 @@
 <template>
     <div>
+        <div class="dragndrop__status" v-if="files.length">
+            <ul class="list-inline">
+                <li class="list-inline__item">Files: {{ files.length }}</li>
+                <li class="list-inline__item">Percentage: x%</li>
+                <li class="list-inline__item list-inline__item--last">Time Remaining: 00:00</li>
+            </ul>
+        </div>
         <file v-for="file in files"
               :file="file"
         >
@@ -8,15 +15,22 @@
 </template>
 
 <script>
-    import File from "./File";
+	import File from "./File";
 
-    export default {
-    	props: [
-    		'files'
-        ],
+	export default {
+		props: [
+			'files'
+		],
 
-        components: {
-    		File
-        }
-    }
+		components: {
+			File
+		}
+	}
 </script>
+
+<style>
+    .dragndrop__status {
+        text-align: center;
+        padding: 20px;
+    }
+</style>
